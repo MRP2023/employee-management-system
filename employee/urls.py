ca5de5e95ed8e02c5bee4e23e83133021ce1261e
Alvee3120 <fazlahkarim2002@gmail.com>
2025-11-19 12:39:29 +0600

    esdsdw

--- employee/urls.py
+++ employee/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet
+from .views import EmployeeViewSet,EmployeeDetailByUserView
 
 
 router = DefaultRouter()
 router.register('', EmployeeViewSet , basename='employee')
 
 urlpatterns = [
+    path('details/<int:user_id>', EmployeeDetailByUserView.as_view(), name='employee-detail-by-user'),
     path('',include(router.urls))
+    
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,generics,permissions
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 from .models import Employee
-from .serializers import EmployeeSerializer
+from . models import User
+from .serializers import EmployeeSerializer, UserEmployeeDetailSerializer
 
 # Create your views here.
 
@@ -9,4 +12,26 @@ from .serializers import EmployeeSerializer
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
     
+    
+class EmployeeDetailByUserView(generics.GenericAPIView):
+   
+    serializer_class = UserEmployeeDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    
+
+    def get(self, request, user_id):
+        try:
+            user = User.objects.get(id = user_id)
+        except User.DoesNotExist:
+            raise NotFound("User Not Found")
+        
+        try:
+            employee = user.employee_profile
+        except Employee.DoesNotExist:
+            raise NotFound("Employee Profile Not Found nnnnn")
+        
+        serializer = self.serializer_class(employee)
+        return Response(serializer.data)
